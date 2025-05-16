36bed4f4fc3cfe38849dd0fd4afccb1398dd913d
iqbal-mn694 <garenahiber134@gmail.com>
2025-05-16 10:38:14 +0700

    make helper for pdf generator

--- controllers/main.js
+++ controllers/main.js
@@ -1,23 +1,32 @@
+const { signAndSendTransaction, verifyCertificate } = require("../helpers/blockchain");
 const { signDocument, verifyDocument } = require("../helpers/digitalSignature");
 
 exports.home = (req, res)=> {
   res.send("Hello world")
 }
 
-exports.generateSignedDocument = (req, res) => {
+exports.generateSignedDocument = async (req, res) => {
   const { name, course } = req.body;
   const document = `${name} | ${course}`
 
   // sign document
   const { signature, hash } = signDocument(document);
 
-  res.json({ signature, hash });
+  // send to blockchain
+  const { raw_transaction, block } = await signAndSendTransaction(hash, name, course)
+
+
+  res.json({ signature, hash, raw_transaction, block });
 }
 
 exports.verifyingSignedDocument = (req, res) => {
-  const { name, course, signature } = req.body;
+  const { name, course, signature, hash } = req.body;
   const document = `${name} | ${course}`;
 
+  // verify signature
   const signedDocument = verifyDocument(document, signature);
-  res.json({ signedDocument });
+
+  // verify on blockchain
+  const verifiedCertificate = verifyCertificate(hash);
+  res.json({ signedDocument, verifiedCertificate });
 }
